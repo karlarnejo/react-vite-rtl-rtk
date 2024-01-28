f5a29f529bf98e5ed9c7f5313b932e1a9d5452f0
Karl Anthony Arnejo <karlanthonyarnejo@gmail.com>
2024-01-28 13:43:42 +0800

    fix: fixed icon not being clickable along with the text

--- app/frontend/src/components/Link/Link.tsx
+++ app/frontend/src/components/Link/Link.tsx
@@ -8,15 +8,10 @@ export interface ILinkProps {
 
 export const Link: React.FC<ILinkProps> = ({ icon, label, routeTo }): React.JSX.Element => {
     return (
-        <div className="flex flex-row items-center">
-            <div>
-                <svg className="w-4 h-4 mr-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={icon}/>
-                </svg>
-            </div>
-            <div>
-                <ReactRouterDomLink className="hover:underline" to={routeTo}>{label}</ReactRouterDomLink>
-            </div>
-        </div>
+        <ReactRouterDomLink className="flex flex-row items-center hover:underline" to={routeTo}>
+            <svg className="w-4 h-4 mr-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={icon}/>
+            </svg>{label}
+        </ReactRouterDomLink>
     );
 }
